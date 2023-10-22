e236a5a042b8e1d4abaf7915bda05bb7c4fa6f9f
Camilo <camilogomezr25@gmail.com>
2023-10-21 19:57:39 -0500

    Añadir main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+
     $("#consultar").on("click", function(){
-        alert($("#txt_buscador").val())
+        $.ajax({
+
+            url: "http://127.0.0.1:3002/chat",
+            type: "POST",
+            dataType: "json",
+            contentType: "application/json",
+            data: JSON.stringify({pregunta: $("#txt_buscador").val()}), //convertir el valor a json gracias a stringify
+            
+            success: function (data) { //data es la respuesta
+                console.log(data)
+
+            }
+        })
+    
     })
 })
